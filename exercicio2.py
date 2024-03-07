88af0f835b5cd78735577953c764e563589508fa
XavierDavidm <162513578+XavierDavidm@users.noreply.github.com>
2024-03-06 22:02:34 -0300

    Add files via upload

---
+++ exercicio2.py
@@ -0,0 +1,4 @@
+# .2-Escreva um programa em linguagem Python que solicite o nome do usuário e, em seguida,
+#imprima uma mensagem de boas-vindas na tela, utilizando o nome fornecido.
+nome = input('Digite seu Nome:')
+print('Bem vindo',nome)
