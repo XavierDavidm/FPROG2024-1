93dbc4e58a82817b7f465dc47477c5d0f30a8f86
XavierDavidm <162513578+XavierDavidm@users.noreply.github.com>
2024-03-06 21:27:14 -0300

    lista de exercícios 1

---
+++ listaexercicios1.py
@@ -0,0 +1,32 @@
+# Lista de exercicíos 1
+
+# .1-Utilizando o OnlineGDB, pesquise e implemente um programa que escreva na tela “Olá
+#Mundo!” em 3 linguagens de programação diferentes. Qual é o comando de saída de
+#dados nestas 3 linguagens?
+# o comando em java é print
+# o comando em go é fmt.println
+# o comando em c# é Console.writeLine
+
+# .2-Escreva um programa em linguagem Python que solicite o nome do usuário e, em seguida,
+#imprima uma mensagem de boas-vindas na tela, utilizando o nome fornecido.
+nome = input('Digite seu Nome:')
+print('Bem vindo',nome)
+
+# .3-Escreva um programa em Python que realize o seguinte procedimento:
+#a. Imprima na tela a seguinte questão: Qual é o verdadeiro nome do super-herói
+#Batman?
+#b. Apresente cinco alternativas para o usuário, cada uma em uma linha: a) Bruce
+#Wayne b) Clark Kent c) Peter Parker d) Tony Stark e) Steve Rogers
+#c. Armazene a letra correspondente à resposta correta (‘a’) em uma variável.
+#d. Solicite ao usuário que digite sua resposta, e a armazene em uma variável.
+#e. Ao final, o programa deve exibir na tela a resposta do usuário e a resposta correta.
+#Por exemplo, se o usuário digitou como resposta a alternativa ‘d’, a mensagem
+#seria esta:
+#Você respondeu alternativa d. A resposta correta é a alternativa a
+
+print('Quiz: Qual é o verdadeiro nome ddo super-herói batman?')
+print('A) Bruce Wayne')
+print('B) Clark Kent')
+print('C) Peter Parker')
+print('D) Tony Stark')
+print('E)')
