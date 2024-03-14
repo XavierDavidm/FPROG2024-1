63e73c7e78f85c520949733734f5625f9757f9dc
XavierDavidm <davidmoreirax15@hotmail.com>
2024-03-13 21:35:00 -0300

    ex1 lista de exercicios 1

--- listaExercicios2.py
+++ listaExercicios2.py
@@ -1,7 +1,9 @@
 #.1- Faça um algoritmo que leia uma quantidade de tempo em minutos e escreva o tempo equivalente 
 #em segundos na tela.
 
-
+minutos = int(input('quantos minutos?'))
+segundos = minutos*60
+print(minutos, 'minutos equivalem','a',segundos,'segundos')
 
 
 
