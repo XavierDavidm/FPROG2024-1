a288f097fb952062836d831d74b0add70af67178
XavierDavidm <davidmoreirax15@hotmail.com>
2024-03-13 21:57:25 -0300

    Update listaExercicios2.py

--- listaExercicios2.py
+++ listaExercicios2.py
@@ -15,6 +15,14 @@ quantidade = float(input('quantos dolares deseja comprar?'))
 total=dolarcotacao*quantidade
 print('voce precisa pagar',total,'reais para ter',quantidade,'dolares!')
 
+#.3- Um restaurante de buffet a quilo cobra R$ 40,00 por quilo. Escreva um programa que leia o peso 
+#do prato do cliente e calcule o valor a ser pago.
+
+porquilo=40.00
+peso=float(input('informe o peso do prato: '))
+valorbuffet=peso*porquilo
+print('voce devera pagar',valorbuffet,'reais por',peso,'quilos de comida')
+
 
 
 
