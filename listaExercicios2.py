0319567683818957027a92d1567362077156bd3e
XavierDavidm <davidmoreirax15@hotmail.com>
2024-03-16 16:22:05 -0300

    Update listaExercicios2.py

--- listaExercicios2.py
+++ listaExercicios2.py
@@ -23,9 +23,21 @@ peso=float(input('informe o peso do prato: '))
 valorbuffet=peso*porquilo
 print('voce devera pagar',valorbuffet,'reais por',peso,'quilos de comida')
 
+#.4- Faça um algoritmo que permita ao aluno calcular a sua média final na Unisinos. Leia a nota do grau
+#A e do grau B e escreva o resultado na tela. Lembrando que o Grau A vale 1/3 e o Grau B 2/3
 
+mediaA=float(input('digite sua nota do grau A: '))
+mediaB=float(input('digite sua nota do grau B: '))
+mediaFinal= round(mediaA*1/3 + mediaB*2/3)
+print('sua media A foi ',mediaA,'sua media B foi ',mediaB,'então sua media final é',mediaFinal)
 
-
+#.5- Um motorista deseja encher o tanque do seu carro com gasolina. Escreva um algoritmo para ler o
+#preço do litro da gasolina e o valor que o motorista tem disponível para abastecer. Calcule quantos
+#litros ele conseguiu colocar no tanque e exiba na tela.
+precoLitroGasolina=5.50
+dinheiroMotorista=float(input('digite o valor disponivel: '))
+totalLitros=round(dinheiroMotorista/precoLitroGasolina)
+print('voce colocou',totalLitros,'Litros de gasolina no tanque')
 
 
 
