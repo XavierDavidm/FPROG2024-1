d5307b59e154947e7cea8f58cedea696f028549e
XavierDavidm <davidmoreirax15@hotmail.com>
2024-03-20 21:18:32 -0300

    Update listaExercicios3.py

--- listaExercicios3.py
+++ listaExercicios3.py
@@ -10,10 +10,39 @@ else:
 
 #.2- Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor que A
 #+ C.
-A=float(input('digite o valor de A'))
-B=float(input('digite o valor de B'))
-C=float(input('digite o valor de C'))
+A=float(input('digite o valor de A: '))
+B=float(input('digite o valor de B: '))
+C=float(input('digite o valor de C: '))
 if A+B<A+C:
     print('A+B é menor que A+C')
 else:
     print('A+B é maior que A+C')
+
+#.3- Encontrar o dobro de um número caso ele seja positivo e o seu triplo caso seja negativo, imprimindo
+#o resultado
+numero=float(input('digite um numero positivo ou negativo: '))
+if numero >0:
+    final=numero*2
+    print('o dobro de',numero,'é',final)
+else:
+    final=numero*3
+    print('o triplo de',numero,'é',final)
+
+#.4- Crie um programa que verifica se um número inteiro informado pelo usuário é divisível por 3.
+numerodivisivel=int(input('informe um numero para verificar se é divisivel por 3: '))
+if numerodivisivel%3==0:
+    print(numerodivisivel,'é divisivel por 3')
+else:
+    print(numerodivisivel,'não é divisivel por 3')
+
+
+
+
+
+
+
+
+
+
+
+
