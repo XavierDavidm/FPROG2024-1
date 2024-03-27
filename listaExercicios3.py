f95714392c1ebb9eb30288951da4d2e1c715c671
XavierDavidm <davidmoreirax15@hotmail.com>
2024-03-27 19:36:53 -0300

    continuaçao lista exercicios 3

--- listaExercicios3.py
+++ listaExercicios3.py
@@ -49,7 +49,8 @@ else:
 #soma for par OU se ele escolheu ÍMPAR e o valor da soma é ímpar, diga que ele venceu. Senão, diga
 #que o programa venceu
 import random
-aposta=int(input('você aposta em 2 ou 1: '))
+
+aposta=int(input('você aposta em 0 ou 1: '))
 numDoUser=int(input('digite um numero de 0 a 5: '))
 sorteio=random.randint(0,5)
 print('o computador jogou',sorteio)
@@ -59,9 +60,19 @@ if resultadoDaAposta%aposta==0:
     print('você ganhou')
 else:
     print('você perdeu')
-    
 
-
+#.7-Implementar um programa que calcula o desconto previdenciário de um funcionário. O programa
+#deve, dado um salário retornar o valor do desconto proporcional ao mesmo. O cálculo de desconto
+#segue a regra: o desconto deve 11% do valor do salário. Entretanto, o valor máximo de desconto é
+#318,20. Sendo assim, ou o método retorna 11% sobre o salário ou 318,20.
+salario=float(input('informe seu salário: '))
+taxaDeDesconto= 0.11
+salDesc=salario*taxaDeDesconto
+if salDesc > 318.20:
+    descontoPrevidencia=318.20
+else:
+    descontoPrevidencia=salDesc
+print('para um salario de',salario,'reais é aplicado',descontoPrevidencia,'reais para o desconto previdenciário')
 
 
 
