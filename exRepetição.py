68afa7e91a2af7bce105f5f0acb3ed52ba70ff6d
XavierDavidm <davidmoreirax15@hotmail.com>
2024-03-27 21:51:55 -0300

    Update exRepetição.py

--- exRepetição.py
+++ exRepetição.py
@@ -7,7 +7,17 @@ while resposta != 'S' and resposta != 'N':
         print('você respondeu não')
     else:
         print('resposta invalida')
+
+#enquanto o user não digitar S ou N o programa repete a pergunta usando a condição while
    
+#contador com while: escrever o nome 10 vezes
+nome=input('digite seu nome: ')
+#variavel para contar quantidade de vezes que o nome foi escrito 
+cont=0
+while cont<10:
+    print(nome)
+    cont=cont+1 #incremento em 1 unidade
+    
 
 
 
