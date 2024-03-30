7b1e32ab73e28b4d82c1488c120e94eb8200f3d2
XavierDavidm <davidmoreirax15@hotmail.com>
2024-03-29 21:47:06 -0300

    lista 03 exercicio 13 and 14

--- test.py
+++ test.py
@@ -1,41 +1,45 @@
-totalNotasUser=int(input('informe a quantia que deseja trasformar em cédulas: '))
-notas100=0
-notas50=0
-notas20=0
-notas10=0
-notas5=0
-notas1=0
+notaGA=-1
+notaGB=-1
+while notaGA < 0 or notaGB <0 or notaGA > 10 or notaGB > 10:
+    notaGA=float(input('digite sua nota do grau A: '))
+    notaGB=float(input('digite sua nota do grau B: '))
+    mediaF=round(notaGA*1/3 + notaGB*2/3)
+    if notaGA < 0 or notaGB < 0 or notaGA > 10 or notaGB > 10:
+        print('nota invalida!, por favor informe uma nota de 0 a 10')
+    elif mediaF >=6:
+        print('sua média final foi',mediaF)
+        print('você foi aprovado, parabéns!')
+    else:
+        print('sua média final foi',mediaF)
+        print('Você não foi aprovado, deverá realizar a recuperação(grau C)')
+        notaGC=-1
+        while notaGC < 0 or notaGC >10:
+            notaGC=float(input('digite sua nota do grau C (recuperação): '))
+            if notaGC < 0 or notaGC >10:
+                print('nota invalida!, por favor informe uma nota de 0 a 10')
+            else:
+                escolharec=1
+                while escolharec !='a' and escolharec !='b':
+                    escolharec=input('escolha qual grau deseja substuir (a) ou (b): ')
+                    if escolharec !='a' and escolharec!='b':
+                        escolharec=1
+                        print('por favor escolha qual grau será substituido (a) ou (b)')
+                    else:
+                        if escolharec =='a':
+                            mediaF=round(notaGC*1/3 + notaGB*2/3)
+                            escolharec='a'
+                            if mediaF >= 6:
+                                print('você foi aprovado com',mediaF,'média parabéns!')
+                            else:
+                                print('sua média final foi',mediaF)
+                                print('você foi reprovado mesmo com o grau C')
+                        elif escolharec =='b':
+                            mediaF=round(notaGA*1/3 + notaGC*2/3)
+                            escolharec='b'
+                            if mediaF >= 6:
+                                print('você foi aprovado com',mediaF,'média parabéns!')
+                            else:
+                                print('sua média final foi',mediaF)
+                                print('você foi reprovado mesmo com o grau C')
 
-while totalNotasUser>=100:
-    totalNotasUser=totalNotasUser-100
-    notas100=notas100+1
-while totalNotasUser>=50:
-    totalNotasUser=totalNotasUser-50
-    notas50=notas50+1
-while totalNotasUser>=20:
-    totalNotasUser=totalNotasUser-20
-    notas20=notas20+1
-while totalNotasUser>=10:
-    totalNotasUser=totalNotasUser-10
-    notas10=notas10+1
-while totalNotasUser>=5:
-    totalNotasUser=totalNotasUser-5
-    notas5=notas5+1
-while totalNotasUser>=1:
-    totalNotasUser=totalNotasUser-1
-    notas1=notas1+1
-
-if notas100 > 0:
-    print(notas100,'nota(s) de R$ 100')
-if notas50 > 0:
-    print(notas50,'nota(s) de R$ 50')
-if notas20 > 0:
-    print(notas20,'nota(s) de R$ 20')
-if notas10 > 0:
-    print(notas10,'nota(s) de R$ 10')
-if notas5 > 0:
-    print(notas5,'nota(s) de R$ 5')
-if notas1 > 0:
-    print(notas1,'nota(s) de R$ 1')
-if notas1==0 and notas5==0 and notas10==0 and notas20==0 and notas50==0 and notas100==0:
-    print('você não digitou um valor valido para converter em cédulas')
+                
