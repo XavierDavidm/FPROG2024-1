f0bb0121498f2be54e567c4507c0f06cf5787575
XavierDavidm <davidmoreirax15@hotmail.com>
2024-04-03 21:02:23 -0300

    ex3

--- exRepetição.py
+++ exRepetição.py
@@ -17,7 +17,33 @@ cont=0
 while cont<10:
     print(nome)
     cont=cont+1 #incremento em 1 unidade
-    
+
+#Faça um programa que permita o usuário
+#entrar com uma senha no máximo em 3
+#tentativas
+#– Quais são as condições para a repetição?
+#• Quando a repetição deve parar?
+senhaCadastrada=123
+senha=-10
+tentativas=0
+while senhaCadastrada != senha and tentativas <=3:
+    senha=int(input('digite sua senha: '))
+    if senhaCadastrada == senha:
+        print('olá sua senha esta correta!')
+    elif tentativas >=2:
+        print('você digitou a senha incorreta 3 vezes, aguarde 1h para tentar novamente')
+        tentativas=4
+    else:
+        print('senha invalida tente novamente!')
+        tentativas = tentativas + 1
+
+# modelo simples sem print da quantidade de tentativas restantes
+
+
+#comando for (VARIAVEL) in range
+counter=0
+for counter in range(10):
+    print(counter)
 
 
 
--- test.py
+++ test.py
@@ -1,28 +1,19 @@
-etiqueta=-1
-while etiqueta < 0:
-    etiqueta=float(input('digite o valor da etiqueta do produto: '))
-    if etiqueta < 0:
-        print('valor do produto inválido, por favor registre o valor correto')
-formaDePagamento=-1
-while formaDePagamento!='dinheiro' and formaDePagamento!='cartão' and formaDePagamento!='cartão2x' and formaDePagamento!='cartão3x':
-    print('À vista em dinheiro(15% de desconto)',          '(dinheiro)') 
-    print('À vista no cartão de crédito(10% de desconto)', '(cartão)')
-    print('Em duas vezes(sem juros)',                      '(cartão2x)')
-    print('Em três vezes(juros de 10%)',                   '(cartão3x)')
-    formaDePagamento=input('digite a forma de pagamento que deseja (por favor digite a opção entre parenteses): ')
-    if formaDePagamento=='dinheiro':
-        totalfinalproduto=etiqueta*0.85
-        print('você pagou em dinheiro à vista',totalfinalproduto,'reais')
-    elif formaDePagamento=='cartão':
-        totalfinalproduto=etiqueta*0.90
-        print('você pagou no cartão à vista',totalfinalproduto,'reais')
-    elif formaDePagamento=='cartão2x':
-        totalfinalproduto=etiqueta
-        print('você pagou no cartão em duas vezes',totalfinalproduto,'reais')     
-    elif formaDePagamento=='cartão3x':
-        totalfinalproduto=etiqueta*1.10
-        print('você pagou no cartão em tres vezes',totalfinalproduto,'reais')
+senhaCadastrada=123
+senha=-10
+tentativas=0
+check=True
+tentativasR=3
+while senhaCadastrada != senha and check!=True:
+    senha=int(input('digite sua senha: '))
+    if senhaCadastrada == senha:
+        print('olá sua senha esta correta!')
+        check=True
+    elif tentativas >3:
+        print('você digitou a senha incorreta 3 vezes, aguarde 1h para tentar novamente')
+        check=True
     else:
-        print('forma de pagamento inválida, por favor insira novamente a forma de pagamento que deseja de acordo com a tabela a seguir')
-
-                
+        print('senha invalida tente novamente!')
+        tentativas = tentativas + 1
+        tentativasR=tentativasR-tentativas
+        print(tentativasR,'restantes')
+        check=False
