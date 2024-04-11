90876ecfa2dc5cea05d79a0018a00ff8d7ee762a
XavierDavidm <davidmoreirax15@hotmail.com>
2024-04-11 15:58:51 -0300

    Update listaExercicios4.py

--- listaExercicios4.py
+++ listaExercicios4.py
@@ -1,7 +1,32 @@
-#.1- 
+#lista 04 while e for
+#.1 Faça um algoritmo para:
+#a. Gerar e escrever todos os números inteiros do intervalo [0,100].
+#b. Gerar e escrever os números pares do intervalo [20,50].
+#c. Gerar e escrever os números inteiros do intervalo [25,70] em ordem decrescente.
+#d. Gerar e escrever os números ímpares do intervalo [25,95] em ordem decrescente.
+#e. Ler 15 números e escrever a soma e a média dos números lidos.
+#f. Ler 10 números inteiros e escrever a quantidade de números pares e a quantidade de números
+#ímpares lidos.
+#g. Sortear 20 números inteiros entre -10 e 10 e imprimi-los acompanhados da mensagem
+#“POSITIVO”, “NEGATIVO”, ou “NULO”, conforme o caso. No final, imprimir a quantidade de
+#números positivos e negativos lidos.
+#h. Ler n números e imprimir no final a soma dos números lidos (obs.: n é a quantidade de números
+#que deverão ser lidos e também deve ser lido do teclado)
 
+#1a)
+for A in range(0,101):
+    print(A)
+print('#####')
 
+#1B)
+for B in range(20,51,2):
+    print(B)
+print('#####')
 
+#1C)
+for C in range(70,24,-1):
+    print(C)
+print('#####')
 
 
 
