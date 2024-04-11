ffcfe58ee966e99b43c4e9f71fef6d2e5557c515
XavierDavidm <davidmoreirax15@hotmail.com>
2024-04-11 12:43:19 -0300

    tabuada

--- funçõesExemplos.py
+++ funçõesExemplos.py
@@ -43,7 +43,36 @@ funParametros(False,True,False)
 #podem ser mistos os parametros/lembrando que em outras linguagens tem que especificar a tipagem,
 # ex funçao(int p1,float p2,string p3)
 
+# exemplos elefantes 
 
+#função#
+def musicaElefantes(n):
+    for i in range(1,n):
+        print(i,'elefante(s) incomanda(m) muito a gente,')
+        print(i+1,'elefante(s) incomanda(m)', end='')
+        for j in range(0,i+1):
+            print('incomodam, ', end='')
+        print('muito mais!')
+
+#main#
+n=int(input('numero de elefantes: '))
+musicaElefantes(n)
+
+#retorno função
+#normamente usada para calculos ou quando queremos true ou false
+#ex3
+#area funções#
+def mediaUni(grauA, grauB):
+    media=(grauA+ 2*grauB)/3.0
+    return media
+
+#main#
+grauA=float(input('digite a media grau A'))
+grauB=float(input('digite a media grau B'))
+
+grauFinal= mediaUni(grauA,grauB)
+print(grauFinal,'é sua media final')
+#usando retorno e função para fazer e dizer a media 
 
 
 
---
+++ test.py
@@ -0,0 +1,21 @@
+def tabuada(n):
+    for i in range(1,11):
+        res=n*i
+        print(n,'x',i,'=',res)
+#main#
+n=int(input('digite um número para saber a tabuada dele: '))
+tabuada(n)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
