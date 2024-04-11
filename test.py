30cae245ed4852048ccc29bb96d1c87ed11b8722
XavierDavidm <davidmoreirax15@hotmail.com>
2024-04-11 17:22:57 -0300

    ex1 lista04

--- test.py
+++ test.py
@@ -1,21 +1,15 @@
-def tabuada(n):
-    for i in range(1,11):
-        res=n*i
-        print(n,'x',i,'=',res)
-#main#
-n=int(input('digite um número para saber a tabuada dele: '))
-tabuada(n)
-
-
-
-
-
-
-
-
-
-
-
+num=0
+numR=0
+nLido=0
+numS=0
+n=int(input('informe quantos números devem ser lidos: '))
+print(n,'números serão lidos abaixo')
+while nLido<n:
+    numR=float(input('digite um número: '))
+    nLido=nLido+1
+    print(nLido,')',numR)
+    numS=numS+numR
+print('a soma de todos os',n,'números é',numS)
 
 
 
