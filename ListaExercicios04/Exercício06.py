adfa79337cfab2b0f95486585147fe59513aa1d9
XavierDavidm <davidmoreirax15@hotmail.com>
2024-04-12 22:03:44 -0300

    06 refeito lista 04

--- ListaExercicios04/Exercício06.py
+++ ListaExercicios04/Exercício06.py
@@ -2,48 +2,23 @@
 
 #6 -Crie um algoritmo que sorteie 5 valores entre 0 e 100 e depois imprima o menor, o maior e a média
 #dos valores sorteados.
-
-#area import
 import random
+def oGrandeSorteio():
+    valoresSorteio= []
 
-#area function
+    for i in range(0,5):
+        valor=random.randint(0,100)
+        valoresSorteio.append(valor)
 
-#area variaveis
-n1=0
-n2=0
-n3=0
-n4=0
-n5=0
+    maior=max(valoresSorteio)
+    menor=min(valoresSorteio)
+    soma=sum(valoresSorteio)
+    media=soma/5
 
-#MAIN#
-n1=random.randint(0,100)
-n2=random.randint(0,100)
-n3=random.randint(0,100)
-n4=random.randint(0,100)
-n5=random.randint(0,100)
+    print(maior,'é o maior número sorteado')
+    print(menor,'é o menor número sorteado')
+    print(soma,'é a soma entre os números sorteados')
+    print(media,'é a média entre os números sorteados')
 
-print(n1,n2,n3,n4,n5)
-if n1>n2 and n1>n3 and n1>n4 and n1>n5:
-    print(n1,'é o maior numero sorteado')
-elif n2>n1 and n2>n3 and n2>n4 and n2>n5:
-    print(n2,'é o maior numero sorteado')
-elif n3>n1 and n3>n2 and n3>n4 and n3>n5:
-    print(n3,'é o maior numero sorteado')
-elif n4>n1 and n4>n3 and n4>n2 and n4>n5:
-    print(n4,'é o maior numero sorteado')
-elif n5>n1 and n5>n3 and n5>n4 and n5>n2:
-    print(n5,'é o maior numero sorteado')
-
-if n1<n2 and n1<n3 and n1<n4 and n1<n5:
-    print(n1,'é o menor numero sorteado')
-elif n2<n1 and n2<n3 and n2<n4 and n2<n5:
-    print(n2,'é o menor numero sorteado')
-elif n3<n1 and n3<n2 and n3<n4 and n3<n5:
-    print(n3,'é o menor numero sorteado')
-elif n4<n1 and n4<n3 and n4<n2 and n4<n5:
-    print(n4,'é o menor numero sorteado')
-elif n5<n1 and n5<n3 and n5<n4 and n5<n2:
-    print(n5,'é o menor numero sorteado')
-    
-media=(n1+n2+n3+n4+n5)/5
-print('a média entre todos os números sorteados é:',media)
+#executar
+oGrandeSorteio()
---
+++ colinhacomandos.py
@@ -0,0 +1,7 @@
+#min
+#max
+#print
+#if else elif 
+#while for(in range)
+#and or 
+# variavel=[] inicia uma lista
