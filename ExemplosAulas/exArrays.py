05870c4aedbb27eacd4aceed2d70e1e61b5a19f5
XavierDavidm <davidmoreirax15@hotmail.com>
2024-05-29 20:56:52 -0300

    Update exArrays.py

---
+++ ExemplosAulas/exArrays.py
@@ -0,0 +1,40 @@
+#exemplos arrays/vetor
+#bibliotecas array, numpy, list para python
+#1- criando array simples de 10
+
+#array A
+A=[1,2,3,4,5,6,7,8,9,10]
+
+#descobrir tamanho
+tamanho = len(A) #ou 
+print(len(A))
+print('o tamanho do array é de',tamanho)
+
+#somente em python podemos exibir tudo dentro do array só printando ele
+print(A)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
