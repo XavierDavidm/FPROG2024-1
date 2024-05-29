bde1168adc89844fbb7280532cc995b582e13dad
XavierDavidm <davidmoreirax15@hotmail.com>
2024-05-29 19:35:45 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
+def div(n,m):
+    count = 0
+    num = 1
+    while count < 5:
+        if num % n == 0 and num % m == 0:
+            print(num)
+            count += 1
+        num += 1
 
 
-
-
-
-
-
-
+div(2,5)
     
