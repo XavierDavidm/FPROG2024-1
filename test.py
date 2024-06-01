a1b3603016b5649e74a0e70e50a8d7eeed2b8902
XavierDavidm <davidmoreirax15@hotmail.com>
2024-06-01 13:24:13 -0300

    lista 06 completa

--- test.py
+++ test.py
@@ -1,12 +1,5 @@
-def div(n,m):
-    count = 0
-    num = 1
-    while count < 5:
-        if num % n == 0 and num % m == 0:
-            print(num)
-            count += 1
-        num += 1
 
-
-div(2,5)
-    
+p=10/200
+r=p*100
+#rint(p)
+print(r)
