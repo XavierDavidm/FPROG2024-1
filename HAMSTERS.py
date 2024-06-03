84da32c2005282b53df6b0427e4a0a543b94a591
XavierDavidm <davidmoreirax15@hotmail.com>
2024-06-03 17:30:29 -0300

    ham

---
+++ HAMSTERS.py
@@ -0,0 +1,22 @@
+import random
+
+
+
+#gerador de array vazio(0) para as posições baseado no número de hamsters
+#função usada sempre que iniciar uma nova corrida
+def iniciar():
+    N=int(input('digite o número de hamsters'))
+    POS=[0]*N
+    return POS
+
+
+POS=iniciar()
+print(POS)
+
+
+
+
+
+
+
+
