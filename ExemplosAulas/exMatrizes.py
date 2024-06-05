2cae0cc6de483e09cb5e0129e3cd593f1986877b
XavierDavidm <davidmoreirax15@hotmail.com>
2024-06-05 20:32:11 -0300

    Update exMatrizes.py

--- ExemplosAulas/exMatrizes.py
+++ ExemplosAulas/exMatrizes.py
@@ -1,4 +1,4 @@
-#matriz
+#matriz(tabela) ou (lista de listas)
     #0  #1  #3  #4 ---->colulas
 M=[[12, 11, 10, 9], #0->linha
    [8,  7,  6,  5], #1
@@ -8,8 +8,8 @@ M=[[12, 11, 10, 9], #0->linha
 #M=[[12,11,10,9],[8,7,6,5],[4,3,2,1]]
 
 print(M)
-print(len(M))
-print(len(M[0]))
+print(len(M))    #-->n de linhas(n de arrays)
+print(len(M[0])) #-->n de colunas(n de elementos)
 
 
 
